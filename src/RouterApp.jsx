038959f7361e022b9ae138097cdce026a8a6af08
asmajati15 <asmajati.cooler15@gmail.com>
2024-08-31 13:21:45 +0700

    fix

--- src/RouterApp.jsx
+++ src/RouterApp.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from './pages/Home'
+import Maintence from './pages/Maintence'
 import './App.css'
 
 function RouterApp() {
   return (
     <BrowserRouter>
     <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='*' element={<Home />} />
     </Routes>
   </BrowserRouter>
   )
