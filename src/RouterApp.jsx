acb170308f0e32c6d45fe9d2954b780007c0be85
zeru-program <justinebogor0609@gmail.com>
2024-09-03 11:35:24 +0700

    fix maintence

--- src/RouterApp.jsx
+++ src/RouterApp.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from './pages/Home'
-import Maintence from './pages/Maintence'
+// import Maintence from './pages/Maintence'
 import './App.css'
 
 function RouterApp() {
